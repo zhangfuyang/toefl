ad9273c2fb36fb9803ef74c49f4a8f15103829bc
zhangzhuo <zhangzhuo@buaa.edu.cn>
2018-01-03 22:58:06 +0800

    意见反馈可以提交了

--- toefl/Form1.Designer.cs
+++ toefl/Form1.Designer.cs
@@ -89,6 +89,7 @@
             this.indTpo3 = new System.Windows.Forms.Button();
             this.indTpo2 = new System.Windows.Forms.Button();
             this.indTpo1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -101,7 +102,7 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.addtpoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.edittpoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -139,6 +140,7 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
             this.splitContainer1.Panel2.Controls.Add(this.button2);
             this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Panel2.Controls.Add(this.listBox1);
@@ -820,6 +822,16 @@
             this.indTpo1.Text = "tpo1";
             this.indTpo1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 71);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "我的作文";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(17, 111);
@@ -939,15 +951,15 @@
             this.edittpoToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
             this.edittpoToolStripMenuItem.Text = "修改tpo";
             // 
-            // button2
+            // button3
             // 
-            this.button2.Location = new System.Drawing.Point(17, 174);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(28, 71);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "我的作文";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button3.Location = new System.Drawing.Point(24, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 25);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "意见反馈";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -1063,6 +1075,7 @@
         private System.Windows.Forms.ToolStripMenuItem edittpoToolStripMenuItem;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- toefl/feedback.cs
+++ toefl/feedback.cs
@@ -16,5 +16,52 @@ namespace toefl
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sc1 = textBox1.Text;
+            string sc="";
+            string[] ssts = sc1.Split('\'');
+            for (int i = 0; i < ssts.Count(); i++)
+            {
+                if (i != 0)
+                {
+                    sc += "''";
+                }
+                sc += ssts[i];
+            }
+
+            int result;
+            string cm1 = richTextBox1.Text;
+            string cm = "";
+            string[] sts = cm1.Split('\'');
+            for (int i = 0; i < sts.Count(); i++)
+            {
+                if (i != 0)
+                {
+                    cm += "''";
+                }
+                cm += sts[i];
+            }
+            string sql = "INSERT INTO [dbo].[UserSuggestion]([name],[suggest],[date],[title])  ";
+            sql += "VALUES('"+SystemConfig.name+"','"+cm +"','"+DateTime.Now +"','"+sc+"')";
+            try
+            {
+                result = DatabaseHelp.executeCommand(sql);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("评分失败，请联系管理员");
+                return;
+            }
+            if (result == 0)
+            {
+                MessageBox.Show("评分失败，请联系管理员");
+                return;
+            }
+            MessageBox.Show("评分成功");
+            DialogResult = DialogResult.OK;
+
+        }
     }
 }
